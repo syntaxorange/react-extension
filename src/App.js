742254cedf3f452294bc6e99906950df2d2e1798
syntaxorange <syntaxorange@gmail.com>
2022-12-22 21:35:07 +0300

    add show url

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [url, setUrl] = useState('');
+
+    /**
+     * Get current URL
+     */
+  useEffect(async () => {
+    const queryInfo = {active: true, lastFocusedWindow: true};
+
+    const tabs = await chrome.tabs.query(queryInfo);
+    const url = tabs[0].url;
+    setUrl(url);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo"/>
+        <p>URL:</p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {url}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
